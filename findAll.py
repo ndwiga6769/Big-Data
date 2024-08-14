6325d105dc3e75ab22d51369f16326223846ffdc
ndwiga6769 <your.email@example.com>
2024-08-14 14:57:21 +0300

    codes

---
+++ findAll.py
@@ -0,0 +1,10 @@
+import re
+
+s2 = "Michael Jackson was a singer and known as the 'King of Pop'"
+
+
+# Use the findall() function to find all occurrences of the "as" in the string
+result = re.findall("as", s2)
+
+# Print out the list of matched words
+print(result)
