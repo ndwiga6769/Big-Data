9cee7de24daab80eb3b3166bc9d11a2dc57a38bf
ndwiga6769 <your.email@example.com>
2024-10-02 17:47:42 +0300

    codes

---
+++ intervie questions/Max and min salary per dept.sql
@@ -0,0 +1,26 @@
+drop table if exists employee;
+create table employee
+(
+	id 			int primary key GENERATED ALWAYS AS IDENTITY,
+	name 		varchar(100),
+	dept 		varchar(100),
+	salary 		int
+);
+insert into employee values(default, 'Alexander', 'Admin', 6500);
+insert into employee values(default, 'Leo', 'Finance', 7000);
+insert into employee values(default, 'Robin', 'IT', 2000);
+insert into employee values(default, 'Ali', 'IT', 4000);
+insert into employee values(default, 'Maria', 'IT', 6000);
+insert into employee values(default, 'Alice', 'Admin', 5000);
+insert into employee values(default, 'Sebastian', 'HR', 3000);
+insert into employee values(default, 'Emma', 'Finance', 4000);
+insert into employee values(default, 'John', 'HR', 4500);
+insert into employee values(default, 'Kabir', 'IT', 8000);
+
+select * from employee;
+
+select *,
+	max(salary) over(partition by dept order by salary desc) as maximum,
+	min(salary) over(partition by dept order by salary desc
+range between unbounded preceding and unbounded following) as minimum,
+	from employee
